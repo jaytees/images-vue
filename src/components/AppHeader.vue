72917897996163722b0c46e0c91475df4907aa05
Jordan Trickett <jordan1trickett@gmail.com>
2020-06-16 23:13:46 +0100

    boilerplate

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="ui secondary pointing menu">
+    <a href="/" class="active item">Image Storage</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style lang="css" scoped></style>
